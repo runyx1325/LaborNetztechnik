5f99220bbd7c97183489d357c251f954d872a816
runyx1325 <lucaburst11@gmail.com>
2023-03-16 00:37:22 +0100

    basic link class

--- link.py
+++ link.py
@@ -1,4 +1,19 @@
 
 class linkClass():
-    def __init__(self):
-        pass
+    def __init__(self, node1, node2, linkWeight):
+        self.node1 = node1
+        self.node2 = node2
+        self.weight = linkWeight
+
+    def __str__(self):
+        return self.node1+"-"+self.node2
+
+    @property
+    def get_weight(self):
+        return self.weight
+    @property
+    def get_node1(self):
+        return self.node1
+    @property
+    def get_node2(self):
+        return self.node2
