f19c4f715c3e182085bdf43cbcd00b739b58a6c7
runyx1325 <lucaburst11@gmail.com>
2023-03-15 23:45:54 +0100

    outsource hardcoded strings

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
 import re
 from settings import settingsClass
 from helper import *
+from node import nodeClass
+from link import linkClass
 
 if __name__ == '__main__':
     settings = settingsClass()
-    #Node: Aaaa = 10101001;
-    nodePattern = r'^([a-zA-Z][a-zA-Z0-9]{0,3}) = (\d+);$'
-    #Link: Aaaa - Bbbb = 1010021010;
-    linkPattern = r'^([a-zA-Z][a-zA-Z0-9]{0,3}) - ([a-zA-Z][a-zA-Z0-9]{0,3}) : (\d+);$'
     #first read: all nodes
-    with open(settings._SettingsDict['inputFile'], "r") as file:
+    nodeDict = {}
+    with open(settings.inputfile, "r") as file:
         for line in file:
             line = line.strip()
             if line.startswith("//") or line.startswith("#"):
                 continue
-            elif re.match(nodePattern, line):
-                nodeName = re.match(nodePattern, line).group(1)
-                nodeID = re.match(nodePattern, line).group(2)
-                if validateNodeData(nodeName, nodeID):
+            elif re.match(settings.nodePattern, line):
+                nodeName = re.match(settings.nodePattern, line).group(1)
+                nodeID = re.match(settings.nodePattern, line).group(2)
+                if validateNodeData(nodeName, nodeID, nodeDict, settings):
                     #create Node
-                    pass
+                    nodeDict[nodeName] = nodeClass(nodeName, nodeID)
+
 
     #second read: all links
-    with open(settings._SettingsDict['inputFile'], "r") as file:
+    with open(settings.inputfile, "r") as file:
         for line in file:
             line = line.strip()
             if line.startswith("//") or line.startswith("#"):
                 continue
-            if re.match(linkPattern, line):
-                linkNode1 = re.match(linkPattern, line).group(1)
-                linkNode2 = re.match(linkPattern, line).group(2)
-                linkWeight = re.match(linkPattern, line).group(3)
-                if validateLinkData(linkNode1, linkNode2, linkWeight):
+            if re.match(settings.linkPattern, line):
+                linkNode1 = re.match(settings.linkPattern, line).group(1)
+                linkNode2 = re.match(settings.linkPattern, line).group(2)
+                linkWeight = re.match(settings.linkPattern, line).group(3)
+                if validateLinkData(linkNode1, linkNode2, linkWeight, nodeDict, settings):
+                    node1 = nodeDict[linkNode1]
+                    node2 = nodeDict[linkNode2]
                     #create Link
-                    pass
+                    linkClass(node1, node2, linkWeight)
+
 
--- settings.py
+++ settings.py
@@ -1,6 +1,17 @@
-#Hier kommen Konstanten und anderer Hardcoe hin
+#Hier kommen Konstanten und anderer Hardcode hin
 class settingsClass():
     def __init__(self):
+        self.inputFile = "input.txt"
+        # Node: Aaaa = 10101001;
+        self.nodePattern = r'^([a-zA-Z][a-zA-Z0-9]{0,3}) = (\d+);$'
+        # Link: Aaaa - Bbbb = 1010021010;
+        self.linkPattern = r'^([a-zA-Z][a-zA-Z0-9]{0,3}) - ([a-zA-Z][a-zA-Z0-9]{0,3}) : (\d+);$'
+        self.MAX_IDENT = 3              #max length of node name
+        self.MAX_ITEMS = 10             #max lines in inputfile
+        self.MAX_KOSTEN = 50            #0 - max costs for edge
+        self.MAX_NODE_ID = 10000        #1 - max node id
+
+
         self._SettingsDict = {
             'inputFile': "input.txt",
             'MAX_IDENT': 3,
