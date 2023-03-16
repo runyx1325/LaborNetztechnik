aa0e4de4d826e3985224254c15f52861525560aa
Luca <lucaburst11@gmail.com>
2023-03-16 13:25:33 +0100

    add countBPDU

--- settings.py
+++ settings.py
@@ -3,10 +3,11 @@ class settingsClass():
     def __init__(self):
         self.inputFile = "input.txt"
         self.MAX_IDENT = 3              #max length of node name
-        self.MAX_ITEMS = 25             #max lines in inputfile
+        self.MAX_ITEMS = 30             #max lines in inputfile
         self.MAX_KOSTEN = 50            #0 - max costs for edge
         self.MAX_NODE_ID = 10000        #1 - max node id
         # Node: Aaaa = 10101001;
         self.nodePattern = r'^([a-zA-Z][a-zA-Z0-9]{0,' + str(self.MAX_IDENT) + r'}) = (\d+);$'
         # Link: Aaaa - Bbbb = 1010021010;
         self.linkPattern = r'^([a-zA-Z][a-zA-Z0-9]{0,' + str(self.MAX_IDENT) + r'}) - ([a-zA-Z][a-zA-Z0-9]{0,' + str(self.MAX_IDENT) + r'}) : (\d+);$'
+        self.countBPDU = 0
